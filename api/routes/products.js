b6b96e3a993b095bedaa1732ffbee5c274077ba6
Mohamed.Yehia <mohamed.yehia>
2020-11-10 23:52:33 +0200

    Adding product api

---
+++ api/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) =>{
+   res.status(200).json({
+    message: 'handling get request for products!'
+   });
+});
+
+router.post('/', (req, res, next) =>{
+    res.status(200).json({
+        message: 'handling post request for products!'
+    });
+});
+
+router.get('/:productId', (req, res, next) =>{
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'good job!',
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            message: 'u selected an id!'
+        });
+    } 
+});
+
+router.put('/:productId', (req, res, next) =>{
+    res.status(200).json({
+        message: 'Updated successfully!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) =>{
+    res.status(200).json({
+        message: 'Deleted successfully!'
+    });
+});
+
+module.exports = router;
