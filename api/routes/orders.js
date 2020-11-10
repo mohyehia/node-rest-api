877c8c3bea2c4e0bd440c42894b6c968e019bd17
Mohamed.Yehia <mohamed.yehia>
2020-11-11 00:04:46 +0200

    adding orders api

---
+++ api/routes/orders.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) =>{
+    res.status(200).json({
+        message: 'Orders were fetched!'
+    });
+});
+
+router.post('/', (req, res, next) =>{
+    res.status(201).json({
+        message: 'Order was created!'
+    });
+});
+
+router.get('/:orderId', (req, res, next) =>{
+    res.status(200).json({
+        message: 'order details!',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderI', (req, res, next) =>{
+    res.status(200).json({
+        message: 'order deleted!',
+        orderId: req.params.orderId
+    });
+})
+
+module.exports = router;
