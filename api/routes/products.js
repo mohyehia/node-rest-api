d28b28a397da1d9dd2a12bb006bb1a2d28bcaca5
Mohamed.Yehia <mohamed.yehia>
2020-11-11 15:18:22 +0200

    parsing body & handling cors error

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(201).json({
-        message: 'handling post request for products!'
+        message: 'handling post request for products!',
+        createdProduct: product
     });
 });
 
