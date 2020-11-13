eb0b114c74997942c58c406803886597a53aedca
Mohamed.Yehia <mohamed.yehia>
2020-11-13 13:59:09 +0200

    Adding mongoose to our api to save data to db

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,46 +1,123 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../entity/product');
+const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) =>{
-   res.status(200).json({
-    message: 'handling get request for products!'
-   });
+    Product.find()
+    .select('_id name price')
+    .then(products =>{
+        res.status(200).json({
+            count: products.length,
+            products: products.map(prod =>{
+                return {
+                    id: prod._id,
+                    name: prod.name,
+                    price: prod.price,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:5000/products/' + prod._id
+                    }
+                }
+            })
+        });
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.post('/', (req, res, next) =>{
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
-    res.status(201).json({
-        message: 'handling post request for products!',
-        createdProduct: product
+    });
+    product.save().then(prod =>{
+        console.log(prod);
+        res.status(201).json({
+            message: 'Product created successfully!',
+            createdProduct: {
+                id: prod._id,
+                name: prod.name,
+                price: prod.price,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:5000/products/' + prod._id
+                }
+            }
+        });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: err});
     });
 });
 
 router.get('/:productId', (req, res, next) =>{
     const id = req.params.productId;
-    if(id === 'special'){
-        res.status(200).json({
-            message: 'good job!',
-            id: id
-        });
-    }else{
-        res.status(200).json({
-            message: 'u selected an id!'
-        });
-    } 
+    Product.findById(id)
+    .select('id name price')
+    .then(prod =>{
+        console.log(prod);
+        if(prod){
+            res.status(200).json({
+                product: prod,
+                request: {
+                    type: 'GET',
+                    description: 'Get all products',
+                    url: 'http://localhost:5000/products'
+                }
+            });
+        }else{
+            res.status(404).json({message: 'Not Found!'});
+        }
+    })
+    .catch(err =>{
+        console.error(err);
+        res.status(500).json({error: err});
+    })
 });
 
 router.put('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Updated successfully!'
+    const id = req.params.productId;
+    Product.update({_id: id}, {$set: {name: req.body.name, price: req.body.price}})
+    .then(result =>{
+        console.log(result);
+        res.status(200).json({
+            message: 'Product updated successfully!',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:5000/products/' + id
+            }
+        });
+    })
+    .catch(err =>{
+        console.error(err);
+        res.status(500).json({erorr: err});
     });
 });
 
 router.delete('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Deleted successfully!'
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .then(result =>{
+        console.log(result);
+        res.status(200).json({
+            message: 'Product deleted!',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:5000/products',
+                description: 'Create new product',
+                body: {
+                    name: 'String', price: 'Number'
+                }
+            }
+        });
+    })
+    .catch(err =>{
+        console.error(err);
+        res.status(500).json({error: err});
     });
 });
 
--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connect', () =>{
+    console.log('connected to mongo db!');
+});
+mongoose.connection.on('error', (err) =>{
+    console.error(`error connecting to mongo db: ${err}`);
+});
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
